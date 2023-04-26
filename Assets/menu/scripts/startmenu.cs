bb4df81323a717aba88d6bb7ace53b663a373d41
Aster <queyue.qy777@gmail.com>
2023-04-26 20:03:11 +1000

    HomePage added

---
+++ Assets/menu/scripts/startmenu.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startmenu : MonoBehaviour
+{
+    public void selectingmenu()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
+// Start is called before the first frame update
+     
+    }
+
+
